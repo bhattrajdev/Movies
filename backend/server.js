0d153b5b1990558e6d4f7094627c39b780fdab67
bhattrajdev <devrajbhatt010@gmail.com>
2023-09-24 20:18:48 +0545

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 
 import express from "express";
 import dotenv from 'dotenv'
+import connectDB from "./config/db.js";
 
 const app = express();
 app.use(express.json())
 dotenv.config()
+
+connectDB()
 
 const PORT = process.env.PORT || 4000
 
