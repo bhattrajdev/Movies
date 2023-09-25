5c21ac4cf8fe2eef05aa91e561eacc0dc2acbeba
bhattrajdev <devrajbhatt010@gmail.com>
2023-09-26 00:11:50 +0545

    get movie route

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import Movies from "../models/movieModel";
 
 
-// const getMovies = (res,req) =>{
-// const movies = 
-// }
+const getMovies =async (res,req) =>{
+const movies = await Movies.find({})
+res.json(movies)
+}
